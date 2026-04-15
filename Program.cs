859e839930c09a35e81763c9a2cb07073b2fbd7d
DAlbu03 <DAlbu03@users.noreply.github.com>
2026-04-15 16:37:57 +0200

    create new dot net project

---
+++ Program.cs
@@ -0,0 +1,36 @@
+using dar_system.Data;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllersWithViews();
+builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+    .AddCookie(options =>
+    {
+        options.LoginPath = "/login";
+        options.LogoutPath = "/logout";
+        options.AccessDeniedPath = "/";
+    });
+builder.Services.AddDbContext<DarDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DarConnection")));
+
+var app = builder.Build();
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+app.UseRouting();
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
